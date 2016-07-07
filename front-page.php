d3b4f113ad8ed7d417c409b8ac1b533446016eb3
Henry Hadlow <henry@manofscience.co.uk>
2016-07-07 16:14:36 +0100

    Move second image section to just above footer

--- front-page.php
+++ front-page.php
@@ -34,7 +34,7 @@ function workstation_front_page_genesis_meta() {
    			$classes[] = 'front-page';
 
   			return $classes;
-  
+
 		}
 
 		//* Force full width content layout
@@ -59,8 +59,8 @@ function workstation_front_page_widgets() {
 
 	$image_section_2 = get_option( '2-workstation-image', sprintf( '%s/images/bg-2.jpg', get_stylesheet_directory_uri() ) );
 
-	if ( ! empty( $image_section_1 ) ) {	
-		echo '<div class="image-section-1"></div>';	
+	if ( ! empty( $image_section_1 ) ) {
+		echo '<div class="image-section-1"></div>';
 	}
 
 	genesis_widget_area( 'front-page-1', array(
@@ -78,14 +78,14 @@ function workstation_front_page_widgets() {
 		'after'  => '</div></div>',
 	) );
 
-	if ( ! empty( $image_section_2 ) ) {
-		echo '<div class="image-section-2"></div>';
-	}
-
 	genesis_widget_area( 'front-page-4', array(
 		'before' => '<div id="front-page-4" class="front-page-4"><div class="flexible-widgets widget-area wrap' . workstation_widget_area_class( 'front-page-4' ) . '">',
 		'after'  => '</div></div>',
 	) );
+
+  if ( ! empty( $image_section_2 ) ) {
+    echo '<div class="image-section-2"></div>';
+  }
 
 }
 
